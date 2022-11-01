c6e42223a9791d5090177a321eb861b19fa39a13
virginia-oc <94895896+virginia-oc@users.noreply.github.com>
2022-11-01 16:52:17 +0100

    Zenchan jumps done

--- Assets/Scripts/BubbleShot.cs
+++ Assets/Scripts/BubbleShot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpPoint : MonoBehaviour
+public class BubbleShot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,13 +14,5 @@ public class JumpPoint : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "ZenChan")
-        {
-            FindObjectOfType<ZenChan>().SendMessage("Saltar");
-        }
     }
 }
--- Assets/Scripts/ZenChan.cs
+++ Assets/Scripts/ZenChan.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ZenChan : MonoBehaviour
 {
     private float alturaPersonaje;
     private float anchoPersonaje;
     private float velocidadX = -0.5f;
-    private float velocidadSalto = 8f;
-    private float constante;
+    private float constante;   
+    private Vector3 localScale;
 
     // Start is called before the first frame update
     void Start()
@@ -16,60 +17,37 @@ public class ZenChan : MonoBehaviour
         alturaPersonaje = GetComponent<Collider2D>().bounds.size.y;
         anchoPersonaje = GetComponent<Collider2D>().bounds.size.x;
         constante = anchoPersonaje / 2 + anchoPersonaje / 8;
+        localScale = transform.localScale;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 vectorDere = new Vector2(
-            transform.position.x + constante, transform.position.y);
-        Vector2 vectorIzq = new Vector2(
-            transform.position.x - constante, transform.position.y);
-        RaycastHit2D hitIzq = Physics2D.Raycast(vectorDere, new Vector2(0, -1));
-        RaycastHit2D hitDer = Physics2D.Raycast(vectorIzq, new Vector2(0, -1));
-        
-        float distanciaAlSueloIzq = hitIzq.distance;
-        float distanciaAlSueloDer = hitDer.distance;
-        bool tocandoElSueloIzq = distanciaAlSueloIzq < alturaPersonaje;
-        bool tocandoElSueloDer = distanciaAlSueloDer < alturaPersonaje;
-
-        if (tocandoElSueloDer || tocandoElSueloIzq)
-        {
-            transform.Translate(velocidadX * Time.deltaTime, 0, 0);
-            //System.Random r = new System.Random();
-            //int aleatorio = r.Next(1, 11);
-            //Debug.Log(aleatorio);                             
-        }           
-        else
-            transform.Translate(0, velocidadX * Time.deltaTime, 0);
-       
-    }
-
-    private void CambiarDireccion()
-    {
-        velocidadX *= -1;
+        transform.Translate(velocidadX * Time.deltaTime, 0, 0);       
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<Collider2D>().tag == "Walls")
         {
-            CambiarDireccion();
+            velocidadX *= -1;
+            localScale.x *= -1;
+            transform.localScale = localScale;
         }
-        else if (collision.gameObject.GetComponent<Collider2D>().tag == "Bubblun")
+        if (collision.gameObject.GetComponent<Collider2D>().tag == "Bubblun")
         {
             FindObjectOfType<GameController>().SendMessage("PerderVida");
             FindObjectOfType<Bubblun>().SendMessage("ResetPosition");
         }
-        //else if (collision.gameObject.GetComponent<Collider2D>().tag == "Respawn")
-        //{
-        //    Saltar();
-        //}
     }
 
-    private void Saltar()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector3 fuerzaSalto = new Vector3(0, velocidadSalto, 0);
-        GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
+        if (collision.tag == "JumpPoint")
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 150f);
+        }
     }
+
 }
